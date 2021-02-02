df6d9710ac37a066c28cd08b010a91318e859988
Prasanth-518 <prasanthvg3@gmail.com>
2021-02-02 13:18:33 +0530

    Configured main.tf to create aks cluster

--- main.tf
+++ main.tf
@@ -17,36 +17,31 @@ provider "azurerm"{
     features{}
 }
 
-resource "azurerm_resource_group" "rg"{
-    name= "TestTerraform"
-    location="eastus"
+resource "azurerm_kubernetes_cluster" "aks"{
+    name= "ContainerTraining"
+    resource_group_name = "Cloud-DevOps-Training"
+    location = "eastus"
+    dns_prefix = "ContainerTraining-dns"
+
+    default_node_pool{
+        name = "default"
+        node_count = 2
+        vm_size = "Standard_DS3_v2"
+    }
+    identity{
+        type = "SystemAssigned"
+    }
 }
-# resource "azurerm_kubernetes_cluster" "aks"{
-    # name= "ContainerTraining"
-    # resource_group_name = "Cloud-DevOps-Training"
-    # location = "eastus"
-    # dns_prefix = "ContainerTraining-dns"
-# 
-    # default_node_pool{
-        # name = "default"
-        # node_count = 2
-        # vm_size = "Standard_DS3_v2"
-    # }
-    # identity{
-        # type = "SystemAssigned"
-    # }
-# }
-# 
-# resource "azurerm_container_registry" "acr"{
-    # name = "prasanth98registry"
-    # resource_group_name = "Cloud-DevOps-Training"
-    # location = "eastus"
-    # sku = "Standard"
-# }
-# 
-# resource "azurerm_role_assignment" "aksToacr"{
-    # scope = azurerm_container_registry.acr.id
-    # role_definition_name = "AcrPull"
-    # principal_id = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
-# }
-# 
+
+resource "azurerm_container_registry" "acr"{
+    name = "prasanth98registry"
+    resource_group_name = "Cloud-DevOps-Training"
+    location = "eastus"
+    sku = "Standard"
+}
+
+resource "azurerm_role_assignment" "aksToacr"{
+    scope = azurerm_container_registry.acr.id
+    role_definition_name = "AcrPull"
+    principal_id = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+}
