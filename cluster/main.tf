bfab62fae7440200514ccddba2569d5ef416037e
Prasanth-518 <prasanthvg3@gmail.com>
2021-04-15 11:39:18 +0530

    Removed the role assignment bt container registry and cluster

--- cluster/main.tf
+++ cluster/main.tf
@@ -39,9 +39,3 @@ resource "azurerm_container_registry" "acr"{
     location = "eastus"
     sku = "Standard"
 }
-
-resource "azurerm_role_assignment" "aksToacr"{
-    scope = azurerm_container_registry.acr.id
-    role_definition_name = "AcrPull"
-    principal_id = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
-}
